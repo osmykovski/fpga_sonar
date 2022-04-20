f66bb30d968531996e4d49b7878c21339ea1eebf
osmykovski <99010855+osmykovski@users.noreply.github.com>
2022-04-20 17:33:02 +0300

    syncronous decimator design

--- ip/decimator/decimator.sv
+++ ip/decimator/decimator.sv
@@ -15,13 +15,6 @@ module decimator (
     output logic               m_axis_tlast
 );
 
-    // TODO: fix timings
-    assign s_axis_tready = word_cnt[s_axis_tuser] ? 1 : m_axis_tready;
-    assign m_axis_tdata = s_axis_tdata;
-    assign m_axis_tuser = s_axis_tuser;
-    assign m_axis_tvalid = word_cnt[s_axis_tuser] ? 0 : s_axis_tvalid;
-    assign m_axis_tlast = s_axis_tlast;
-
     logic [2:0] word_cnt [7:0];
     always @(posedge s_axis_aclk) begin
         if(!s_axis_aresetn)
@@ -29,5 +22,56 @@ module decimator (
         else if(s_axis_tvalid & s_axis_tready)
             word_cnt[s_axis_tuser] <= (word_cnt[s_axis_tuser] + 1) % 5;
     end
+
+    logic [23:0] tdata;
+    logic [2:0] tuser;
+
+    enum logic [1:0] {
+        ST_READ  = 2'b01,
+        ST_WRITE = 2'b10
+    } dec_state;
+
+    always @(posedge s_axis_aclk) begin
+        if (!s_axis_aresetn)
+            dec_state <= ST_READ;
+        else begin
+            case (dec_state)
+                ST_READ: begin
+                    if(s_axis_tvalid & word_cnt[s_axis_tuser] == 4)
+                        dec_state <= ST_WRITE;
+                end
+                // ================================ //
+                ST_WRITE: begin
+                    if(m_axis_tready)
+                        dec_state <= ST_READ;
+                end
+            endcase
+        end
+    end
+
+    always @(posedge s_axis_aclk) begin
+        if(s_axis_aresetn & s_axis_tvalid & s_axis_tready) begin
+            tdata <= s_axis_tdata;
+            tuser <= s_axis_tuser;
+        end
+    end
+    
+    logic [7:0] tlast;
+    always @(posedge s_axis_aclk) begin
+        if(!s_axis_aresetn)
+            tlast <= 0;
+        else begin
+            if(s_axis_tvalid & s_axis_tready & s_axis_tlast)
+                tlast[s_axis_tuser] = 1;
+            if(m_axis_tvalid & m_axis_tready)
+                tlast[m_axis_tuser] = 0;
+        end
+    end
+
+    assign m_axis_tdata = tdata;
+    assign m_axis_tvalid = dec_state == ST_WRITE;
+    assign m_axis_tuser = tuser;
+    assign m_axis_tlast = tlast[tuser];
+    assign s_axis_tready = dec_state == ST_READ;
 
 endmodule
--- ip/decimator/tb.sv
+++ ip/decimator/tb.sv
@@ -14,84 +14,75 @@ module tb ();
         rstn <= 1;
     end
 
-    int fid_out;
-    logic signed [31:0] test_data[0:9999];
-    int fid;
-
-    initial begin
-        fid_out = $fopen("C:/Users/Public/out.bin", "wb");
-        fid = $fopen("wave.bin", "rb");
-        $fread(test_data, fid);
-        $fclose(fid);
-    end
-
     logic [13:0] sample_cnt;
     always @(posedge clk) begin
         if(!rstn)
             sample_cnt <= 0;
         else if(s_tvalid & s_tready)
-            sample_cnt <= (sample_cnt + 1) % 10000;
+            sample_cnt <= sample_cnt + 1;
     end
-
-    assign s_tuser = 0;
 
     always @(posedge clk) begin
         if(sample_cnt == 9999) begin
-            $fclose(fid_out);
             $finish();
         end
     end
 
-    logic [31:0] s_tdata;
+    logic [23:0] s_tdata;
     logic s_tvalid, s_tready;
-    logic [1:0] s_tuser;
+    logic [2:0] s_tuser;
+    logic       s_tlast;
 
-    int tvalid_cnt;
+    always @(posedge clk) begin
+        s_tvalid <= $random();
+    end
+
     always @(posedge clk) begin
         if(!rstn)
-            tvalid_cnt <= 0;
-        else begin
-            if(tvalid_cnt < 20)
-                tvalid_cnt <= tvalid_cnt + 1;
-            else
-                tvalid_cnt <= 0;
-        end
+            s_tuser <= 0;
+        else if(s_tvalid & s_tready)
+            s_tuser <= s_tuser + 1;
     end
-    assign s_tvalid = tvalid_cnt == 20;
 
-    // reversing bit order
-    logic [31:0] data;
     always @(posedge clk) begin
-        s_tdata = {test_data[sample_cnt][7:0], test_data[sample_cnt][15:8], test_data[sample_cnt][23:16], test_data[sample_cnt][31:24]};
+        if(!rstn)
+            s_tdata <= 0;
+        else if(s_tvalid & s_tready)
+            s_tdata <= $random();
     end
 
     logic signed [23:0] m_tdata;
+    logic [2:0] m_tuser;
     logic tvalid;
-
-    always @(posedge clk) begin
-        if(tvalid) begin
-            $fwrite(fid_out, "%c", m_tdata[7:0]);
-            $fwrite(fid_out, "%c", m_tdata[15:8]);
-            $fwrite(fid_out, "%c", m_tdata[23:16]);
-            $fwrite(fid_out, "%c", 0);
-        end
-    end
+    logic m_tlast;
 
     logic tready = 1;
+
+    int tlast_cnt;
+    always @(posedge clk) begin
+        if(!rstn)
+            tlast_cnt <= 0;
+        else if(s_tvalid & s_tready & s_tuser == 7)
+            tlast_cnt <= (tlast_cnt + 1) % 33;
+    end
+
+    assign s_tlast = tlast_cnt == 32;
 
     decimator UUT(
         .s_axis_aclk    (clk),
         .s_axis_aresetn (rstn),
 
-        .s_axis_tdata   (s_tdata[23:0]),
+        .s_axis_tdata   (s_tdata),
         .s_axis_tvalid  (s_tvalid),
         .s_axis_tready  (s_tready),
         .s_axis_tuser   (s_tuser),
+        .s_axis_tlast   (s_tlast),
 
         .m_axis_tdata   (m_tdata),
         .m_axis_tvalid  (tvalid),
         .m_axis_tready  (tready),
-        .m_axis_tuser   ()
+        .m_axis_tuser   (m_tuser),
+        .m_axis_tlast   (m_tlast)
     );
     
 endmodule
