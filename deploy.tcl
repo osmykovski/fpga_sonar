1650cac45dcf3dfbb010d28346d96f507d18503d
osmykovski <99010855+osmykovski@users.noreply.github.com>
2022-04-27 19:24:28 +0300

    deploy script update

--- deploy.tcl
+++ deploy.tcl
@@ -12,7 +12,7 @@ add_files -fileset sim_1 -quiet {sources/tb}
 # * constraints
 add_files -fileset constrs_1 -quiet {sources/xdc}
 # * non-bd ip-cores
-add_files -norecurse [glob sources/ip/*/*.xci]
+add_files -fileset sim_1 -quiet -norecurse [glob sources/ip/*/*.xci]
 
 # ip catalog
 set_property ip_repo_paths {ip} [current_project]
@@ -22,19 +22,3 @@ update_ip_catalog
 add_files -fileset sources_1 -quiet {sources/bd/design_1.bd}
 make_wrapper -files [get_files {sources/bd/design_1.bd}] -top
 add_files -norecurse {sources/bd/hdl/design_1_wrapper.v}
-
-# pre and post scripts
-add_files -fileset utils_1 -quiet [glob sources/script/*_pre.tcl]
-add_files -fileset utils_1 -quiet [glob sources/script/*_post.tcl]
-# * synthesis
-set_property STEPS.SYNTH_DESIGN.TCL.PRE [get_files {sources/script/synth_pre.tcl} -of [get_fileset utils_1]] [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.TCL.POST [get_files {sources/script/synth_post.tcl} -of [get_fileset utils_1]] [get_runs synth_1]
-# * implementation
-set_property STEPS.INIT_DESIGN.TCL.PRE [get_files {sources/script/impl_pre.tcl} -of [get_fileset utils_1]] [get_runs impl_1]
-set_property STEPS.INIT_DESIGN.TCL.POST [get_files {sources/script/impl_post.tcl} -of [get_fileset utils_1]] [get_runs impl_1]
-# * bitstream
-set_property STEPS.WRITE_BITSTREAM.TCL.PRE [get_files {sources/script/bit_pre.tcl} -of [get_fileset utils_1]] [get_runs impl_1]
-set_property STEPS.WRITE_BITSTREAM.TCL.POST [get_files {sources/script/bit_post.tcl} -of [get_fileset utils_1]] [get_runs impl_1]
-
-# project after-deploy settings
-source {sources/script/prj_set.tcl}
